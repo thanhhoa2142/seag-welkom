8677ff3fb658103637e5af6a8056ec314628b1e8
Hoa Nguyen <hoanguyen.rin@gmail.com>
2025-03-15 00:37:53 +1100

    Add autoimport

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export type FormatNumberOption = {
+  minimumFractionDigits?: number;
+  maximumFractionDigits?: number;
+  useGrouping?: boolean;
+};
+
+export function formatNumber(
+  value: number | string = "0.0",
+  decimals?: FormatNumberOption
+) {
+  const {
+    minimumFractionDigits = 0,
+    maximumFractionDigits = 2,
+    useGrouping,
+  } = decimals || {};
+  const factor = Math.pow(10, maximumFractionDigits);
+  const truncatedValue = Math.floor(Number(value) * factor) / factor;
+  return truncatedValue.toLocaleString("en-US", {
+    minimumFractionDigits,
+    maximumFractionDigits,
+    useGrouping,
+  });
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
+import autoImport from "unplugin-auto-import/webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Add SVGR support
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    config.experiments = { layers: true, topLevelAwait: true };
+
+    // Add tree-shaking support for environment variables
+    config.plugins.push(
+      autoImport({
+        // .ts, .tsx
+        include: [/\.tsx?$/],
+        imports: [
+          "react",
+          { from: "sonner", imports: ["toast"] },
+          {
+            from: "@/lib/constants",
+            imports: [],
+          },
+          {
+            from: "@/lib/utils",
+            imports: ["cn", "formatNumber"],
+          },
+        ],
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
