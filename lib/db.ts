e08eb2f61162a5ded8ebb723b56bffd0b66d293f
Hoa Nguyen <hoanguyen.rin@gmail.com>
2025-03-15 19:25:32 +1100

    fix server actions

--- lib/db.ts
+++ lib/db.ts
@@ -7,3 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export const thisUser = prisma.user.findFirst({
+  where: { username: "rinx1000" },
+});
