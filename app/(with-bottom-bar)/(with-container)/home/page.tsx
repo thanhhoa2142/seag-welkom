bd84fd1897d56621bde5eb42e5f84475c1c99e1f
Nhi Nguyen <Nhi1610@hotmail.com>
2025-03-16 14:56:10 +1100

    gap for front page

--- app/(with-bottom-bar)/(with-container)/home/page.tsx
+++ app/(with-bottom-bar)/(with-container)/home/page.tsx
@@ -1,18 +1,20 @@
-import { PageHeader } from "@/components/ui2/header";
-import TipsOfTheDay from "./components/tips-of-the-day";
-import { RecentChallenge } from "@/app/components/map/recent-challenge";
-import { ChallengeList } from "../challenges/components/challenge-list";
-import Badges from "./components/badges";
+/** @format */
+
+import { PageHeader } from '@/components/ui2/header';
+import TipsOfTheDay from './components/tips-of-the-day';
+import { RecentChallenge } from '@/app/components/map/recent-challenge';
+import { ChallengeList } from '../challenges/components/challenge-list';
+import Badges from './components/badges';
 
 export const metadata: Metadata = {
-  title: "Dashboard",
-  description: "Your personal dashboard",
+  title: 'Dashboard',
+  description: 'Your personal dashboard',
 };
 
 export default function DashboardPage() {
   return (
     <>
-      <div className="space-y-4">
+      <div className='space-y-4 gap-1.5'>
         <PageHeader />
         <RecentChallenge />
         <Badges />
